afc6627e01e653b8b1af9c870967c42fa2e94a1d
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-05-16 01:18:13 +0200

    Update and rename src/me/UseRod/BookGUI/Commands/MainCommand.java to src/me/userod/bookgui/commands/MainCommand.java

---
+++ src/me/userod/bookgui/commands/MainCommand.java
@@ -0,0 +1,46 @@
+package me.userod.bookgui.commands;
+
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import me.userod.bookgui.BookGUI;
+import me.userod.bookgui.utils.BookFactory;
+import me.userod.bookgui.utils.Color;
+import me.userod.bookgui.utils.ConfigVariables;
+
+public class MainCommand implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if (sender instanceof Player) {
+			if(args.length > 0 && ConfigVariables.BookSections.contains(args[0)) {
+				for (String bookName : ConfigVariables.BookSections) {
+					String title = "Books." + bookName + ".title";
+					List<String> pages = Color.translate(getStringList("Books." + bookName + ".pages"));
+					ItemStack book = BookFactory.book(title, sender.getName(), pages);
+					me.userod.bookgui.utils.Book.open(book, (Player) sender);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String getString(String section) {
+		FileConfiguration config = BookGUI.getPlugin(BookGUI.class).getConfig();
+		return config.getString(section);
+	}
+	
+	public List<String> getStringList(String section) {
+		FileConfiguration config = BookGUI.getPlugin(BookGUI.class).getConfig();
+		return config.getStringList(section);
+	}
+}
