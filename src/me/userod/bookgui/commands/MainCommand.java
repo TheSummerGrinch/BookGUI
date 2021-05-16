2a3de04804e95212b2fa61c3aa88362db48fdb69
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-05-16 13:07:55 +0200

    Revert "Removed unused import."
    This reverts commit 23a223f3

--- src/me/userod/bookgui/commands/MainCommand.java
+++ src/me/userod/bookgui/commands/MainCommand.java
@@ -2,6 +2,7 @@ package me.userod.bookgui.commands;
 
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
