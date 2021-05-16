83b640e779ddf5c8fed4589c19875da9d67fa419
iUseRod <78047131+iUseRod@users.noreply.github.com>
2021-05-16 07:34:44 +0100

    Update BookGUI.java

--- src/me/userod/bookgui/BookGUI.java
+++ src/me/userod/bookgui/BookGUI.java
@@ -1,4 +1,4 @@
-package me.userod.bookgui; // Package-names use lowercase
+package me.userod.bookgui;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -14,15 +14,15 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.userod.bookgui.commands.MainCommand; // Again, package-names in lowercase
-import me.userod.bookgui.utils.ConfigVariables; // Ditto
+import me.userod.bookgui.commands.MainCommand;
+import me.userod.bookgui.utils.ConfigVariables;
 
 
 public class BookGUI extends JavaPlugin {
 	
 	private FileConfiguration config;
 	private Logger logger;
-	private PluginManager pluginManager; // Names of fields and variables use camelCase.
+	private PluginManager pluginManager;
 	private List<Listener> listeners;
 	private HashMap<String, CommandExecutor> commands;
 	
@@ -33,17 +33,17 @@ public class BookGUI extends JavaPlugin {
 		this.logger = this.getLogger();
 		this.pluginManager = Bukkit.getPluginManager();
 		this.listeners = new ArrayList<Listener>();
-		this.commands = new HashMap<String, CommandExecutor>(); // Probably a bit much. But I assume you plan on extending this.
+		this.commands = new HashMap<String, CommandExecutor>();
 		this.prepareCommands();
 		
-		this.logInfo("Loading Books..."); // Not everything needs to be logged.
+		this.logInfo("Loading Books...");
 		
 		this.commands.forEach((name, commandExecutor) -> {
 			getCommand(name).setExecutor(commandExecutor);
-		});
+		}); // i don't know what's the difference.
 		
 		if(this.isEnabled()) {
-			this.logInfo("Loaded " + ConfigVariables.bookSections.size() / 3 + " Books..."); // What is this? Some kind of black magic? Why the static abuse?
+			this.logInfo("Loaded " + ConfigVariables.bookSections.size() / 3 + " Books...");
 		}
 	}
 	
