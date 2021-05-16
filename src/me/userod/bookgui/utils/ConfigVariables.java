1234c571c9b139023a093e869d308853c3a46093
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-05-16 12:42:40 +0200

    Removed incorrect import.

--- src/me/userod/bookgui/utils/ConfigVariables.java
+++ src/me/userod/bookgui/utils/ConfigVariables.java
@@ -3,26 +3,25 @@ package me.userod.bookgui.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.userod.bookgui.BookGUI;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
-import me.UseRod.BookGUI.MainClass;
-
 public class ConfigVariables {
-	
+
 	static Plugin bgPlugin = BookGUI.getPlugin(BookGUI.class);
 	static FileConfiguration config = bgPlugin.getConfig();
 
 	public static ConfigurationSection books = get("Books");
 	public static List<String> bookSections = getSubSection(books);
-	
+
 	public static ConfigurationSection get(String sectionName) {
 		ConfigurationSection gottenString;
 		gottenString = config.getConfigurationSection(sectionName);
 		return gottenString;
 	}
-	
+
 	public static List<String> getSubSection(ConfigurationSection section) {
 		List<String> subSections = new ArrayList<String>();
 		for(String childSection : section.getKeys(true)) {
